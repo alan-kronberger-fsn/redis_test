87d8c80f9aab3574237a9f9a13d769fe1d4d675d
alan-kronberger-fsn <alan.kronberger@flowershopnetwork.com>
2025-01-16 13:37:36 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <?php
-require_once "vendor/autoload.php";
-use Predis\Client as PredisClient;
+//require_once "vendor/autoload.php";
+//use Predis\Client as PredisClient;
 $url = "http://169.254.169.254/latest/meta-data/instance-id";
 $instance_id = file_get_contents($url);
 
 
 
-$r = new PredisClient([
-    'scheme'   => 'tcp',
-    'host'     => 'sample.15p8r0.clustercfg.use2.cache.amazonaws.com',
-    'port'     => 6379,
-    'password' => '',
-    'database' => 0,
-]);
+//$r = new PredisClient([
+//    'scheme'   => 'tcp',
+//    'host'     => 'sample.15p8r0.clustercfg.use2.cache.amazonaws.com',
+//    'port'     => 6379,
+//    'password' => '',
+//    'database' => 0,
+//]);
 if ($_COOKIE['sessionId']) {
-    $servers = unserialize($r->get($_COOKIE['sessionId']));
+//    $servers = unserialize($r->get($_COOKIE['sessionId']));
     $servers[] = $instance_id;
 } else {
     $id = session_create_id();
     setcookie('sessionId', $id);
     $servers[] = $instance_id;
-    $r->set($_COOKIE['sessionId'], serialize($servers));
+//    $r->set($_COOKIE['sessionId'], serialize($servers));
 }
 
 ?>
