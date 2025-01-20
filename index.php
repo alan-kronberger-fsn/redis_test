09285507b18d55e60230f8e90d6c1a79adb79be8
alan-kronberger-fsn <alan.kronberger@flowershopnetwork.com>
2025-01-20 08:39:15 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -8,12 +8,18 @@ $url = "http://169.254.169.254/latest/meta-data/instance-id";
 $instance_id = file_get_contents($url);
 
 
+$options = array('cluster' => 'redis');
+$parameters = array(
+        'cluster' => array(
+                'default' => array(
+                        'scheme' => 'tcp',
+                        'host' => 'sample.15p8r0.clustercfg.use2.cache.amazonaws.com',
+                        'port' => 6379,
+                ),
+        ),
+);
 
-$r = new PredisClient([
-    'scheme'   => 'tcp',
-    'host'     => 'sample.15p8r0.clustercfg.use2.cache.amazonaws.com',
-    'port'     => 6379
-]);
+$r = new PredisClient($parameters, $options);
 if (isset($_COOKIE['sessionId'])) {
     $id = $_COOKIE['sessionId'];
     $r->append($id, ",{$instance_id}");
